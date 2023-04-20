8cb05d16573b6362485577a018d8702ac2237530
Job Muturi <muirimuturi17@gmail.com>
2023-04-20 14:22:21 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,43 @@ console.log(blogData);
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <Header />
+    <About />
+    <ArticleList />
+
     </div>
   );
+}
+function Header ({name}) {
+  return <>
+    <h1 key={name}>{name}</h1>
+  </>
+}
+function About ({imageSrc, aboutText}) {
+  const image = imageSrc || 'https://via.placeholder.com/215';
+
+  return (
+    <aside>
+      <img src={image} alt="blog logo" />
+      <p>{aboutText}</p>
+    </aside>
+  );
+}
+function ArticleList ({title}) {
+  return (
+    <main>
+      <Article key={title}/>
+    </main>
+  )
+}
+function Article ({title, date, preview}) {
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <p>{preview}</p>
+    </article>
+  )
 }
 
 export default App;
